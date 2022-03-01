8a66490590490f1fbb21f83917bdc1e7ba997e4c
Eric Yantis <yantis1212@outlook.com>
2022-02-28 21:58:47 -0700

    Add: Stripe integration, title, and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51KYMkwDPPl4YHRdTgrFaQ1365x4Rg9M62709jEaf8Eya4MHXc8jVWWhLkShgAvOtzvAQPkMLDQN88xtp2HZejdpV00BGsrgzGp';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Crown Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
